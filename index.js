7404390925d589f7a3903e6e93d45ed1ce4f81db
Fernando Freire <ffreire@thoughtworks.com>
2015-01-03 17:45:32 +0200

    initial parsing of wolfram result

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ var corsOptions = {
 
 var client = new WolframClient(process.env.WOLFRAM_API_KEY);
 
+var parseResult = function (response) {
+  return response.reduce(function (result, pod) {
+    if (pod.$.id === "Result") {
+      result.data = pod.subpod[0].plaintext[0]
+    };
+    if (pod.$.id === "TideMeasurementStation") {
+      result.station = pod.subpod[0].plaintext[0]
+    };
+    return result
+  }, {});
+};
+
 app.get('/:lat/:lon', cors(corsOptions), function(req, res, next){
   client.query({
-  	input: 'tides | ' + req.params.lat + ' ' + req.params.lon,
-  	units: 'metric'
+    input: 'tides | ' + req.params.lat + ' ' + req.params.lon,
+    units: 'metric'
   }, function(err, result) {
-  	if(err) {
-  	  res.status(400).send('Bad Request');
-  	} else {
-  	  res.json(result);
-  	}
+    if(err) {
+      res.status(400).send('Bad Request');
+    } else {
+      res.json(parseResult(result.queryresult.pod));
+    }
   });
 });
 
