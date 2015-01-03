45bc46f0a481f6d00ee8053f54a07f6bd77c257c
Fernando Freire <ffreire@thoughtworks.com>
2015-01-03 19:37:48 +0200

    adds better parsing for tides and station

--- index.js
+++ index.js
@@ -9,13 +9,35 @@ var corsOptions = {
 
 var client = new WolframClient(process.env.WOLFRAM_API_KEY);
 
+var parseTides = function (tideText) {
+  var tides = [];
+  var parser = /(low|high) tide \| (\d+:\d+ (am|pm) \w+)\n\(([^)]+)\) \| (.\d+\.\d+) meters/g;
+  while ((match = parser.exec(tideText)) !== null) {
+    tides.push({
+      type: match[1],
+      time: match[2],
+      height: match[5],
+      when: match[4]
+    });
+  }
+  return tides;
+};
+
+var parseStation = function (stationText) {
+  var match = stationText.match(/station \| (.+)\n.+\n.+position \| (\d+\.\d+ km)/);
+  return {
+    location: match[1],
+    distance: match[2].replace(' ', '')
+  }
+};
+
 var parseResult = function (response) {
   return response.reduce(function (result, pod) {
     if (pod.$.id === "Result") {
-      result.data = pod.subpod[0].plaintext[0]
+      result.tides = parseTides(pod.subpod[0].plaintext[0]);
     };
     if (pod.$.id === "TideMeasurementStation") {
-      result.station = pod.subpod[0].plaintext[0]
+      result.station = parseStation(pod.subpod[0].plaintext[0]);
     };
     return result
   }, {});
@@ -29,6 +51,7 @@ app.get('/:lat/:lon', cors(corsOptions), function(req, res, next){
     if(err) {
       res.status(400).send('Bad Request');
     } else {
+      console.log(result);
       res.json(parseResult(result.queryresult.pod));
     }
   });
